3a38534506ff2fd605ad889f70de92792dadb530
plewakoff <plewakoff@hotmail.com>
2014-07-03 12:38:17 +0300

    create Poni

---
+++ Poni/Poni.js
@@ -0,0 +1,27 @@
+var Poni = (function () {
+    var oxygen;
+    function breath(){}
+
+    function Constructor () {
+        var name;
+        this.getName = function () {
+            return name;
+        };
+
+        function resetName () {
+
+        }
+
+        return this;
+    }
+
+    Constructor.prototype.speed = 25;
+    Constructor.prototype.run = function () {
+
+    };
+    Constructor.eat = function () {
+
+    };
+
+    return Constructor;
+})();
---
+++ Poni/main.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", main, false);
+
+function main () {
+    var star = new Poni();
+    console.dir(star);
+}
